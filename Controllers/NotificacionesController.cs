a47b59be604725bdd9a6591e2fdb8602e8affce3
Nicolas <nicolasjimenezospina0@gmail.com>
2024-05-30 01:03:13 -0500

    Se agrega endpoint para el envio de la notificacion de servicio funerario

--- Controllers/NotificacionesController.cs
+++ Controllers/NotificacionesController.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
-using SendGrid;
-using SendGrid.Helpers.Mail;
 using ms_notificaciones.Models;
+/* using SendGrid;
+using SendGrid.Helpers.Mail; */
 using Amazon;
 using Amazon.SimpleNotificationService;
 using Amazon.SimpleNotificationService.Model;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
 
 
 namespace ms_notificaciones.Controllers;
@@ -13,7 +15,8 @@ namespace ms_notificaciones.Controllers;
 [Route("[controller]")]
 public class NotificacionesController : ControllerBase
 {
-    [Route("correo-bienvenida")]
+
+    /* [Route("correo-bienvenida")]
     [HttpPost]
     public async Task<ActionResult> EnviarCorreoBienvenida(ModeloCorreo datos)
     {
@@ -25,7 +28,7 @@ public class NotificacionesController : ControllerBase
         msg.SetTemplateData(new
         {
             name = datos.nombreDestino,
-            message = "Bienvenido a la comunidad de la funeraria."
+            message = "Bienvenido a la comunidad de la inmobiliaria."
         });
         var response = await client.SendEmailAsync(msg);
         if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
@@ -36,10 +39,10 @@ public class NotificacionesController : ControllerBase
         {
             return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
         }
-    }
+    } */
 
 
-    [Route("correo-recuperacion-clave")]
+    /* [Route("correo-recuperacion-clave")]
     [HttpPost]
     public async Task<ActionResult> EnviarCorreoRecuperacionClave(ModeloCorreo datos)
     {
@@ -62,10 +65,36 @@ public class NotificacionesController : ControllerBase
         {
             return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
         }
+    } */
+
+/* [Route("enviar-correoPqr")]
+    [HttpPost]
+    public async Task<ActionResult> EnviarCorreoPQR(ModeloCorreo datos)
+    {
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
+
+        SendGridMessage msg = this.CrearMensajeBase(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("WELCOME_SENDGRID_TEMPLATE_ID"));
+        msg.SetTemplateData(new
+        {
+            name = datos.nombreDestino,
+            message = "Esta es su nuevla clave... no la comparta."
+        });
+        var response = await client.SendEmailAsync(msg);
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+        {
+            return Ok("Correo enviado a la dirección " + datos.correoDestino);
+        }
+        else
+        {
+            return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
+        }
     }
+ */
 
 
-    [Route("enviar-correo-2fa")]
+    /* [Route("enviar-correo-2fa")]
     [HttpPost]
     public async Task<ActionResult> EnviarCorreo2fa(ModeloCorreo datos)
     {
@@ -101,56 +130,128 @@ public class NotificacionesController : ControllerBase
         var htmlContent = datos.contenidoCorreo;
         var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
         return msg;
-    }
-
-    //Envío funeral service ticket
-
-    [Route("ticket-funeral-service")]
-[HttpPost]
-public async Task<ActionResult> EnviarTicketFuneralService(ModeloCorreo datos)
-{
-    // Obtener la clave API de SendGrid de las variables de entorno
-    var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
-    var client = new SendGridClient(apiKey);
-
-    // Crear el mensaje base utilizando SendGrid
-    SendGridMessage msg = CrearMensajeBase(datos);
-
-    // Asignar el ID de la plantilla para el correo de ticket de servicio funerario
-    msg.SetTemplateId(Environment.GetEnvironmentVariable("TwoFA_SENDGRID_TEMPLATE_ID"));
-
-    // Asignar los datos a la plantilla
-    msg.SetTemplateData(new
-    {
-            nombre = datos.nombreDestino,
-            mensaje = datos.contenidoCorreo,
-            asunto = datos.asuntoCorreo
-    });
-
-    // Enviar el correo electrónico
-    var response = await client.SendEmailAsync(msg);
-
-    // Verificar si el correo fue enviado con éxito
-    if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
-    {
-        return Ok("Correo de ticket de servicio funerario enviado a la dirección " + datos.correoDestino);
-    }
-    else
-    {
-        return BadRequest("Error enviando el correo de ticket de servicio funerario a la dirección: " + datos.correoDestino);
-    }
-}
-
+    } */
 
     // Envío de SMS
+
+    [Route("correo-recuperacion-clave")]
+        [HttpPost]
+        public async Task<ActionResult> EnviarCorreoRecuperacionClave(ModeloCorreo datos)
+        {
+            // Cargar configuración de AWS
+            var awsAccessKeyId = Environment.GetEnvironmentVariable("ACCESS_KEY_AWSSES");
+            var awsSecretAccessKey = Environment.GetEnvironmentVariable("SECRET_KEY_AWSSES");
+
+            using (var client = new AmazonSimpleEmailServiceClient(awsAccessKeyId, awsSecretAccessKey, RegionEndpoint.USEast2))
+            {
+                var sendRequest = new SendEmailRequest
+                {
+                    Source = "nicolas.1701812174@ucaldas.edu.co", // Reemplaza con tu correo verificado en SES
+                    Destination = new Destination
+                    {
+                        ToAddresses = new List<string> { datos.correoDestino }
+                    },
+                    Message = new Message
+                    {
+                        Subject = new Content(datos.asuntoCorreo ?? "Recuperación de Clave"),
+                        Body = new Body
+                        {
+                            Html = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = datos.contenidoCorreo ?? $"<p>Hola {datos.nombreDestino},</p><p>Esta es su nueva clave. No la comparta con nadie.</p>"
+                            },
+                            Text = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = datos.contenidoCorreo ?? $"Hola {datos.nombreDestino},\nEsta es su nueva clave. No la comparta con nadie."
+                            }
+                        }
+                    }
+                };
+
+                try
+                {
+                    var response = await client.SendEmailAsync(sendRequest);
+                    if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        return Ok("Correo enviado a la dirección " + datos.correoDestino);
+                    }
+                    else
+                    {
+                        return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Exception caught sending email: " + ex.Message);
+                }
+            }
+        }
+
+ [Route("enviar-correo-2fa")]
+        [HttpPost]
+        public async Task<ActionResult> EnviarCorreo2fa(ModeloCorreo datos)
+        {
+            // Cargar configuración de AWS
+            var awsAccessKeyId = Environment.GetEnvironmentVariable("ACCESS_KEY_AWSSES");
+            var awsSecretAccessKey = Environment.GetEnvironmentVariable("SECRET_KEY_AWSSES");
+
+            using (var client = new AmazonSimpleEmailServiceClient(awsAccessKeyId, awsSecretAccessKey, RegionEndpoint.USEast2))
+            {
+                var sendRequest = new SendEmailRequest
+                {
+                    Source = "nicolas.1701812174@ucaldas.edu.co", // Reemplaza con tu correo verificado en SES
+                    Destination = new Destination
+                    {
+                        ToAddresses = new List<string> { datos.correoDestino }
+                    },
+                    Message = new Message
+                    {
+                        Subject = new Content(datos.asuntoCorreo ?? "Autenticación de Dos Factores"),
+                        Body = new Body
+                        {
+                            Html = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = datos.contenidoCorreo ?? $"<p>Hola {datos.nombreDestino},</p><p>Su código de autenticación de dos factores es: {datos.contenidoCorreo}</p>"
+                            },
+                            Text = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = datos.contenidoCorreo ?? $"Hola {datos.nombreDestino},\nSu código de autenticación de dos factores es: {datos.contenidoCorreo}"
+                            }
+                        }
+                    }
+                };
+
+                try
+                {
+                    var response = await client.SendEmailAsync(sendRequest);
+                    if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        return Ok("Correo enviado a la dirección " + datos.correoDestino);
+                    }
+                    else
+                    {
+                        return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Exception caught sending email: " + ex.Message);
+                }
+            }
+        }
+
 
     [Route("enviar-sms")]
     [HttpPost]
     public async Task<ActionResult> EnviarSMSNuevaClave(ModeloSms datos)
     {
-        var accessKey = Environment.GetEnvironmentVariable("ACCESS_KEY_AWS");
-        var secretKey = Environment.GetEnvironmentVariable("SECRET_KEY_AWS");
-        var client = new AmazonSimpleNotificationServiceClient(accessKey, secretKey, RegionEndpoint.USEast1);
+        var accessKey = Environment.GetEnvironmentVariable("ACCESS_KEY_AWSSNS");
+        var secretKey = Environment.GetEnvironmentVariable("SECRET_KEY_AWSSNS");
+        var client = new AmazonSimpleNotificationServiceClient(accessKey, secretKey, RegionEndpoint.USEast2);
         var messageAttributes = new Dictionary<string, MessageAttributeValue>();
         var smsType = new MessageAttributeValue
         {
@@ -177,5 +278,60 @@ public async Task<ActionResult> EnviarTicketFuneralService(ModeloCorreo datos)
         }
     }
 
+
+ [Route("enviar-correoPqr")]
+        [HttpPost]
+        public async Task<ActionResult> EnviarCorreoPQR(ModeloCorreo datos)
+        {
+            // Cargar configuración de AWS
+            var awsAccessKeyId = Environment.GetEnvironmentVariable("ACCESS_KEY_AWSSES");
+            var awsSecretAccessKey = Environment.GetEnvironmentVariable("SECRET_KEY_AWSSES");
+
+            using (var client = new AmazonSimpleEmailServiceClient(awsAccessKeyId, awsSecretAccessKey, RegionEndpoint.USEast2))
+            {
+                var sendRequest = new SendEmailRequest
+                {
+                    Source = "nicolas.1701812174@ucaldas.edu.co", // Reemplaza con tu correo verificado en SES
+                    Destination = new Destination
+                    {
+                        ToAddresses = new List<string> { datos.correoDestino }
+                    },
+                    Message = new Message
+                    {
+                        Subject = new Content(datos.asuntoCorreo ?? "Bienvenido a la comunidad de la funeraria"),
+                        Body = new Body
+                        {
+                            Html = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = datos.contenidoCorreo ?? $"<h1>Buen día, {datos.nombreDestino}</h1><p>Bienvenido a la comunidad de la funeraria.</p>"
+                            },
+                            Text = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = datos.contenidoCorreo ?? $"Bienvenido, {datos.nombreDestino}Bienvenido a la comunidad de la funeraria."
+                            }
+                        }
+                    }
+                };
+
+                try
+                {
+                    var response = await client.SendEmailAsync(sendRequest);
+                    if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        return Ok("Correo enviado a la dirección " + datos.correoDestino);
+                    }
+                    else
+                    {
+                        return BadRequest("Error enviando el mensaje a la dirección: " + datos.correoDestino);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Exception caught sending email: " + ex.Message);
+                }
+            }
+        }
 
 }
